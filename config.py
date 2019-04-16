004813e8579c04c2ef5b68bcd1c8c3ec25d337c7
lmdsghero <33367371+lmdsghero@users.noreply.github.com>
2019-04-16 10:22:43 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+class config():
+    embed_dim = 300
+    hidden_dim = 300
+    layers = 1
+    dropout = 0.1
+    seq_in_size=7200
+    fc_dim=100
+    out_dim=2
+    mind_dim = 600
+    man_dim=16
